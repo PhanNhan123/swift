55237d121507c78307588bfae46fde9351829a5b
your_github_username <your_github_email>
2022-11-09 15:50:59 +0700

    uikit

---
+++ lession3/ListViewController/ReminderListViewController+Actions.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+extension ReminderListViewController{
+    @objc func didPressDoneButton(_ sender: ReminderDoneButton) {
+        guard let id = sender.id else{return}
+        completeReminder(with: id)
+    }
+    @objc func didPressAddButton(_ sender:  UIBarButtonItem){
+        let reminder = Reminder(title:"",dueDate: Date.now)
+        let viewController = ReminderViewController(reminder: reminder){[weak self] reminder in
+            self?.add(reminder)
+            self?.updateSnapshot()
+            self?.dismiss(animated: true)
+        }
+        viewController.isAddingNewReminder = true
+        viewController.setEditing(true, animated: false)
+        viewController.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(didCancelAdd(_:)))
+        viewController.navigationItem.title = NSLocalizedString("Add reminder", comment: "Add reminder view controller title")
+        let navigationControler = UINavigationController(rootViewController: viewController)
+        present(navigationControler, animated: true)
+    }
+    @objc func didCancelAdd(_ sender: UIBarButtonItem){
+        dismiss(animated: true)
+    }
+    @objc func didChangeListStyle(_ sender: UISegmentedControl){
+        listStyle = ReminderListStyle(rawValue: sender.selectedSegmentIndex) ?? .today
+        updateSnapshot()
+    }
+}
+
---
+++ lession3Tests/lession3UITests/lession3Tests.swift
@@ -0,0 +1,33 @@
+//
+//  lession3Tests.swift
+//  lession3Tests
+//
+//  Created by Test VPN on 01/11/2022.
+//
+
+import XCTest
+@testable import lession3
+
+class lession3Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
